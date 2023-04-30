71d2a7fbd06cd83e305af0f8b4ee547a38f8bbef
Mike Wilkes <mikewilkes@outlook.com>
2023-04-30 02:02:19 -0700

    Removed commented out code

--- PetShelterAPI.Solution/PetShelterAPI/Models/Pet.cs
+++ PetShelterAPI.Solution/PetShelterAPI/Models/Pet.cs
@@ -5,11 +5,9 @@ namespace PetShelterAPI.Models
         public int PetId { get; set; }
         public string Name { get; set; }
         public string Species { get; set; }
-        // public string Breed { get; set; }
         public string Sex { get; set; }
         public string Color { get; set; }
         public int Age { get; set; }
-        // public string Image { get; set; }
         public string Description { get; set; }
     }
 }
